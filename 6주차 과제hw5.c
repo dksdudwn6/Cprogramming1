a3d75a1f01bd0af2de2474e98141e6461079f1a5
dksdudwn6 <145089941+dksdudwn6@users.noreply.github.com>
2023-10-17 00:31:01 +0900

    Add files via upload

---
+++ 6주차 과제hw5.c
@@ -0,0 +1,12 @@
+﻿'Project6.exe'(Win32): 'C:\Users\dksdu\source\repos\Project6\x64\Debug\Project6.exe'을(를) 로드했습니다. 기호가 로드되었습니다.
+'Project6.exe'(Win32): 'C:\Windows\System32\ntdll.dll'을(를) 로드했습니다. 
+'Project6.exe'(Win32): 'C:\Windows\System32\kernel32.dll'을(를) 로드했습니다. 
+'Project6.exe'(Win32): 'C:\Windows\System32\KernelBase.dll'을(를) 로드했습니다. 
+'Project6.exe'(Win32): 'C:\Windows\System32\vcruntime140d.dll'을(를) 로드했습니다. 
+'Project6.exe'(Win32): 'C:\Windows\System32\ucrtbased.dll'을(를) 로드했습니다. 
+0x38e0 스레드가 종료되었습니다(코드: 0 (0x0)).
+'Project6.exe'(Win32): 'C:\Windows\System32\kernel.appcore.dll'을(를) 로드했습니다. 
+'Project6.exe'(Win32): 'C:\Windows\System32\msvcrt.dll'을(를) 로드했습니다. 
+0xee0 스레드가 종료되었습니다(코드: 0 (0x0)).
+0x525c 스레드가 종료되었습니다(코드: 0 (0x0)).
+'[17248] Project6.exe' 프로그램이 종료되었습니다(코드: 0 (0x0)).
