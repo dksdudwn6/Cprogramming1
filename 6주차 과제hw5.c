482f2f826e11fc3f868938410edeb0cab23eee3b
dksdudwn6 <145089941+dksdudwn6@users.noreply.github.com>
2023-10-17 00:40:59 +0900

    6주차 과제hw5.c

--- 6주차 과제hw5.c
+++ 6주차 과제hw5.c
@@ -1,12 +1,30 @@
-﻿'Project6.exe'(Win32): 'C:\Users\dksdu\source\repos\Project6\x64\Debug\Project6.exe'을(를) 로드했습니다. 기호가 로드되었습니다.
-'Project6.exe'(Win32): 'C:\Windows\System32\ntdll.dll'을(를) 로드했습니다. 
-'Project6.exe'(Win32): 'C:\Windows\System32\kernel32.dll'을(를) 로드했습니다. 
-'Project6.exe'(Win32): 'C:\Windows\System32\KernelBase.dll'을(를) 로드했습니다. 
-'Project6.exe'(Win32): 'C:\Windows\System32\vcruntime140d.dll'을(를) 로드했습니다. 
-'Project6.exe'(Win32): 'C:\Windows\System32\ucrtbased.dll'을(를) 로드했습니다. 
-0x38e0 스레드가 종료되었습니다(코드: 0 (0x0)).
-'Project6.exe'(Win32): 'C:\Windows\System32\kernel.appcore.dll'을(를) 로드했습니다. 
-'Project6.exe'(Win32): 'C:\Windows\System32\msvcrt.dll'을(를) 로드했습니다. 
-0xee0 스레드가 종료되었습니다(코드: 0 (0x0)).
-0x525c 스레드가 종료되었습니다(코드: 0 (0x0)).
-'[17248] Project6.exe' 프로그램이 종료되었습니다(코드: 0 (0x0)).
+#include <stdio.h>
+
+void decimalToBinary(int n) {
+	if (n == 0) {
+		return 0;
+	}
+	decimalToBinary(n / 2);
+	printf("%d", n % 2);
+}
+
+int main() {
+	int num;
+
+	printf("Please enter a number: ");
+	scanf("%d", &num);
+
+	if (num < 0) {
+		printf("Please enter a number : \n");
+	}
+	else if (num == 0) {
+		printf("0 \n");
+	}
+	else {
+		printf(" ", num);
+		decimalToBinary(num);
+		printf("\n");
+	}
+	return 0;
+}
+
