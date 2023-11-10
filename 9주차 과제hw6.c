8cdfe991b3f50a41a45e5c13d863d0c131b0fcc7
dksdudwn6 <145089941+dksdudwn6@users.noreply.github.com>
2023-11-10 10:57:02 +0900

    9주차 과제hw6.c

--- 9주차 과제hw6.c
+++ 9주차 과제hw6.c
@@ -1,1 +1,31 @@
-﻿
+#include <stdio.h>
+
+int main() {
+    int numbers[5];
+    int oddCount = 0, evenCount = 0;
+
+    printf("Please input five integers:\n");
+    for (int i = 0; i < 5; ++i) {
+        scanf("%d", &numbers[i]);
+    }
+
+    printf("Odd numbers:");
+    for (int i = 0; i < 5; ++i) {
+        if (numbers[i] % 2 != 0) {
+            printf(" %d", numbers[i]);
+            oddCount++;
+        }
+    }
+
+    printf("\nEven numbers:");
+    for (int i = 0; i < 5; ++i) {
+        if (numbers[i] % 2 == 0) {
+            printf(" %d", numbers[i]);
+            evenCount++;
+        }
+    }
+
+    printf("\n");
+
+    return 0;
+}﻿
