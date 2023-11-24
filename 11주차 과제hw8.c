3069516d8565846829f2393f41091c21e3ff50d4
dksdudwn6 <145089941+dksdudwn6@users.noreply.github.com>
2023-11-24 13:03:30 +0900

    11주차 과제hw8.c

--- 11주차 과제hw8.c
+++ 11주차 과제hw8.c
@@ -1,6 +1,40 @@
-﻿빌드 시작...
-1>------ 빌드 시작: 프로젝트: Project14, 구성: Debug Win32 ------
-1>Hello.c
-1>Project14.vcxproj -> C:\Users\dksdu\source\repos\Project14\Debug\Project14.exe
-========== 빌드: 1개 성공, 0개 실패, 0개 최신 상태, 0개 건너뜀 ==========
-========== 빌드이(가) 오후 12:59에 시작되었고 00.828 초이(가) 소요됨 ==========
+﻿#include <stdio.h>
+#include <math.h>
+
+double calculateMean(double arr[], int size) {
+    double sum = 0.0;
+
+    for (int i = 0; i < size; ++i) {
+        sum += arr[i];
+    }
+
+    return sum / size;
+}
+
+double calculateStandardDeviation(double arr[], int size) {
+    double mean = calculateMean(arr, size);
+    double sum_squared_diff = 0.0;
+
+    for (int i = 0; i < size; ++i) {
+        double diff = arr[i] - mean;
+        sum_squared_diff += pow(diff, 2);
+    }
+
+    double variance = sum_squared_diff / size;
+    return sqrt(variance);
+}
+
+int main() {
+    double numbers[5];
+
+    printf("Enter 5 real numbers:\n");
+    for (int i = 0; i < 5; ++i) {
+        printf("Enter number %d: ", i + 1);
+        scanf_s("%lf", &numbers[i]);
+    }
+
+    double standardDeviation = calculateStandardDeviation(numbers, 5);
+    printf("Standard Deviation: %lf\n", standardDeviation);
+
+    return 0;
+}
